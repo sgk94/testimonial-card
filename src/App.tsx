c73738da9f0edfae13b6a30e273c917c07e208fc
Shawn Kim <kimsha004@gmail.com>
2025-06-17 18:43:15 -0700

    card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import Card from "./components/Card";
 function App() {
   return (
     <>
-      <Card />
+      <div className="flex justify-center m-8">
+        <Card
+          imageSrc="test"
+          name="bob"
+          userHandle="@bobstuff"
+          description="hello im so cool"
+        />
+      </div>
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,24 @@
-export default function Card() {
-  return <h1 className="text-amber-400">hello red card</h1>;
+type CardProps = {
+  imageSrc: string;
+  name: string;
+  userHandle: string;
+  description: string;
+};
+
+export default function Card({
+  imageSrc,
+  name,
+  userHandle,
+  description,
+}: CardProps) {
+  return (
+    <>
+      <div className="w-[340px] flex flex-col gap-4 bg-white p-6 rounded-lg card-custom-box-shadow">
+        <div className="text-red-400">{imageSrc}</div>
+        <div className="text-red-400">{name}</div>
+        <div className="text-red-400">{userHandle}</div>
+        <div className="text-red-400">{description}</div>
+      </div>
+    </>
+  );
 }
